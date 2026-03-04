6ea9943b4c291f6a81fad118086bcc405e48aef0
baludev3014 <kbalu3013@gmail.com>
2026-03-04 15:20:44 +0530

    first commit added by dev branch i.e messge logged into the console

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("first message logged on the console")
 console.log("second message logged on the console")
 console.log("third message logged on the console")
+console.log("Message logged into the console by the dev branch")
